bc8fb1b4527b44cdb7169329f8ef5cd066459b5e
k-ibaraki <ibaraki@ncdc.co.jp>
2024-03-23 17:50:26 +0900

    ファイルを経由せずにURLを連携

--- app.ts
+++ app.ts
@@ -6,8 +6,6 @@ import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'yaml';
-import * as output from './output.json';
-
 
 // APPを定義
 const app = new cdk.App();
@@ -44,10 +42,9 @@ const createAPIGateway = (apiGatewayStack: cdk.Stack): void => {
   const apiPath = path.join(__dirname, `api-doc/api.yaml`);
   const yamlFile = fs.readFileSync(apiPath).toString();
 
+  // Lambda関数のURLを取得
+  const url = cdk.Fn.importValue('LambdaUrl');
   // yamlファイルの{TARGET_URL}をLambdaのURLに置換
-  const url = output.lambdaApiStack.LambdaUrl;
-  console.log(url);
-
   const replacedYaml = yamlFile.replace(
     /\{TARGET_URL\}/g,
     url,
